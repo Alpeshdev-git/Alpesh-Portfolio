d558ce1ede652b830d7225aaf5b3820a1215b45e
Alpeshdev-git <alpeshchandankhede@gmail.com>
2025-07-10 20:19:28 +0530

    Update frontend

--- frontend/src/Componets/Contact.jsx
+++ frontend/src/Componets/Contact.jsx
@@ -17,7 +17,7 @@ export default function Contact() {
  const handleSubmit = async (e) => {
   e.preventDefault();
   try {
-    const response = await fetch('https://your-app-name.onrender.com/api/contact', {
+    const response = await fetch('https://alpesh-portfolio.onrender.com/api/contact', {
       method: 'POST',
       headers: {
         'Content-Type': 'application/json',
